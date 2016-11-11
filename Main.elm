9d3098dfa6f4ef86b3e87bdcff3498f6c387591d
Marius Skaar Ludvigsen <marius.ludvigsen@gmail.com>
2016-11-11 07:12:02 +0100

    Refactored stuff

--- Main.elm
+++ Main.elm
@@ -14,6 +14,8 @@ import Element
 import Color
 import String
 import Json.Decode as Json
+import Model exposing (..)
+import Toolbar exposing (toolbar)
 
 main =
   Html.program
@@ -24,19 +26,6 @@ main =
   }
 
 -- MODEL
-type alias Model = {
-    x: Float
-  , y: Float
-  , width: Int
-  , height: Int
-  , downs: List (List (Float, Float))
-  , currentColor: String
-  , colors: List String
-  , isDown: Bool
-  , currentStroke: String
-  , strokes: List String
-  }
-
 initialModel: Model
 initialModel =
     { x = 0
@@ -57,14 +46,6 @@ init =
 
 -- UPDATE
 
-type Msg
-    = Position Float Float
-    | DownPosition Float Float
-    | UpPosition Float Float
-    | Size Int Int
-    | ChangeColor String
-    | ChangeStroke String
-    | NoOp
 
 updateLast xs x =
     let
@@ -108,7 +89,7 @@ subscriptions: Model -> Sub Msg
 subscriptions model =
     Sub.batch
         [
-          Mouse.moves (\{x, y} -> Position (toFloat x) (toFloat y))
+          Mouse.moves (\{x, y} -> Model.Position (toFloat x) (toFloat y))
         , Mouse.downs (\{x, y} -> DownPosition (toFloat x) (toFloat y))
         , Mouse.ups (\{x, y} -> UpPosition (toFloat x) (toFloat y))
         , Window.resizes (\{width, height} -> Size width height)
@@ -128,49 +109,14 @@ viewDown down = Html.div [] (map (\(x,y) -> Html.text ((toString x) ++ "," ++ (t
 viewDowns: List (List (Float, Float)) -> Html a
 viewDowns downs = Html.div [] (map viewDown downs)
 
-getColorBoxStyle color =
-    style
-        [
-          ("background", color)
-        , ("width", "2rem")
-        , ("height", "2rem")
-        , ("display", "inline-block")
-        ]
 
-decoder = (always NoOp) 
-
-createColorBox color = Html.div [
-                        (getColorBoxStyle color)
-                       , (onWithOptions "click" {preventDefault = False, stopPropagation = True} (Json.succeed (ChangeColor color)))
-                       ] []
-
-createColorBoxes colors = map createColorBox colors
-
-colors = ["#2a2c2e", "#e20800", "#d0021b", "#5cbd5c", "#5cb85c", "#6dc20f", "#ffffff", "#a8a5a6", "#e34a3e", "#f47f30",
-          "#ffc200", "#91c591", "#c0e3c0", "#4d91e2", "#f5f9fd", "#387ecf", "#4a90e2", "#475b72", "#304155", "#1d7ab7",
-          "#2e71b2", "#223246", "#a0a9ba", "#596479", "#000000", "#dddddd", "#ececec", "#fcfcfc", "#a4a4a4", "#acacac",
-          "#cccccc", "#969696", "#e5e5e5", "#f5f5f5", "#888888", "#e6e5e5", "#e1e1e1", "#ea4ccb", "#f46c30", "#edbc64",
-          "#6495ed", "#f9f0e6"]
-
-rangeSlider v =
-    Html.input
-        [ A.type' "range"
-        , A.min "0"
-        , A.max "40"
-        , value v
-        , onInput ChangeStroke
-        ] []
 
 -- VIEW
 view: Model -> Html Msg
 view model =
-    Html.div [] [
-      Html.div [] [
-           Html.div [] (createColorBoxes colors),
-           Html.input [value model.currentColor, onInput ChangeColor] [],
-           (rangeSlider model.currentStroke)
-          ],
-      svg [ style [ ( "width", (toString (model.width * 2)) ++ "px" ), ( "height", (toString (model.height * 2)) ++ "px" ) ] ]
+    Html.div [A.class "container"] [
+      (toolbar model),
+      svg [ style [ ( "width", (toString (model.width * 1)) ++ "px" ), ( "height", (toString (model.height * 1)) ++ "px" ) ] ]
           [ g [ transform "translate(0,0)" ]
               (map3 makePath model.downs model.colors model.strokes)
           ]
---
+++ Toolbar.elm
@@ -0,0 +1,47 @@
+module Toolbar exposing (toolbar)
+import Html.Attributes as A exposing (style, value)
+import Model exposing (..)
+import Html
+import Html.Events exposing (onInput, onClick, onWithOptions)
+import List exposing (..)
+import Json.Decode as Json
+
+getColorBoxStyle color =
+    style
+        [
+          ("background", color)
+        , ("width", "2rem")
+        , ("height", "2rem")
+        , ("display", "inline-block")
+        ]
+
+decoder = (always NoOp) 
+
+createColorBox color = Html.div [
+                        (getColorBoxStyle color)
+                       , (onWithOptions "click" {preventDefault = False, stopPropagation = True} (Json.succeed (ChangeColor color)))
+                       ] []
+
+createColorBoxes colors = map createColorBox colors
+
+colors = ["#2a2c2e", "#e20800", "#d0021b", "#5cbd5c", "#5cb85c", "#6dc20f", "#ffffff", "#a8a5a6", "#e34a3e", "#f47f30",
+          "#ffc200", "#91c591", "#c0e3c0", "#4d91e2", "#f5f9fd", "#387ecf", "#4a90e2", "#475b72", "#304155", "#1d7ab7",
+          "#2e71b2", "#223246", "#a0a9ba", "#596479", "#000000", "#dddddd", "#ececec", "#fcfcfc", "#a4a4a4", "#acacac",
+          "#cccccc", "#969696", "#e5e5e5", "#f5f5f5", "#888888", "#e6e5e5", "#e1e1e1", "#ea4ccb", "#f46c30", "#edbc64",
+          "#6495ed", "#f9f0e6"]
+
+rangeSlider v =
+    Html.input
+        [ A.type' "range"
+        , A.min "0"
+        , A.max "40"
+        , value v
+        , onInput ChangeStroke
+        ] []
+
+toolbar model =
+    Html.div [A.class "toolbar"] [
+      Html.div [] (createColorBoxes colors),
+      Html.input [value model.currentColor, onInput ChangeColor] [],
+      (rangeSlider model.currentStroke)
+    ]
